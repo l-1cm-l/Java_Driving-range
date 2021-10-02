b2e2ec2eebc1526ed8a74c560dd4df58d5f8681a
{l-1cm-l} <{l-----1.5cm----l@kyf.biglobe.ne.jp}>
2021-10-02 11:32:11 +0900

    [test]

---
+++ MyJava_practice/Testtes.java
@@ -0,0 +1,10 @@
+public class Testtes {
+    public static void main(String[] args){
+        int age = 20;
+        if(20>=age){
+            System.out.println("成年");
+        }else{
+            System.out.println("未成年");
+        }
+    }
+}
