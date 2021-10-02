b8229506c467da161fcabd85746428e81ca50996
{l-1cm-l} <{l-----1.5cm----l@kyf.biglobe.ne.jp}>
2021-10-02 13:22:05 +0900

    [test]

--- MyJava_practice/practice2.java
+++ MyJava_practice/practice2.java
@@ -1,8 +1,4 @@
-public class practice2 {
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
+public class Practice2 {
     public static void main(String[] args){
         int sum = 0;
         for(int i = 0;i<=10;i++){
