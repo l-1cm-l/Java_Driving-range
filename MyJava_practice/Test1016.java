0a1285ed7562b16d8b3ba68164bb4946722925c1
{l-1cm-l} <{l-----1.5cm----l@kyf.biglobe.ne.jp}>
2021-10-16 11:27:33 +0900

    [test]

---
+++ MyJava_practice/Test1016.java
@@ -0,0 +1,18 @@
+public class Test1016{
+    public static void main(String[] args){
+        for(int i=1;i<=100;i++){
+            if(i%3==0&&i%5==0){
+                System.out.println("3と5の倍数");
+            }
+            else if(i%3==0){
+                System.out.println("3の倍数");
+            }
+            else if(i%5==0){
+                System.out.println("5の倍数");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
