d44cfadbeb07e4ad61960e2090f90138295770c3
{l-1cm-l} <{l-----1.5cm----l@kyf.biglobe.ne.jp}>
2021-10-25 09:43:48 +0900

    [practice]

---
+++ MyJava_practice/Test1_2.java
@@ -0,0 +1,8 @@
+public class Test1_2 {
+    public static void main(String[] args){
+        int x = 3;
+        int y = 5;
+        int area = x*y;
+        System.out.println("長方形の面積は" + area);
+    }
+}
