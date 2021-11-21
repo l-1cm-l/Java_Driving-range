830124a87bc7d57bf25c27078f03ea86663e5cbd
{l-1cm-l} <{l-----1.5cm----l@kyf.biglobe.ne.jp}>
2021-11-21 11:47:27 +0900

    [test]

---
+++ rust_test/target/debug/deps/rust_test-6905832fb22b1d2f.d
@@ -0,0 +1,5 @@
+/Users/admin/remoterepositories/rust_test/target/debug/deps/rust_test-6905832fb22b1d2f: src/main.rs
+
+/Users/admin/remoterepositories/rust_test/target/debug/deps/rust_test-6905832fb22b1d2f.d: src/main.rs
+
+src/main.rs:
---
+++ rust_test/target/debug/rust_test.d
@@ -0,0 +1,1 @@
+/Users/admin/remoterepositories/rust_test/target/debug/rust_test: /Users/admin/remoterepositories/rust_test/src/main.rs
